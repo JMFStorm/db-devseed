5b95e7e046f90617935774b62d40c892e1c183d1
Janne Fagerström <jannefagerstrm@pop-os.localdomain>
2024-08-04 19:12:44 +0300

    add windows build

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 BUILD_DIR="./build"
-EXECUTABLE_NAME="dbds"
+EXECUTABLE_NAME_LINUX="dbds"
+EXECUTABLE_NAME_WINDOWS="dbds.exe"
 
+# Create the build directory if it does not exist
 mkdir -p "$BUILD_DIR"
 
-go build -o "$BUILD_DIR/$EXECUTABLE_NAME"
-
-# Check if the build was successful
+# Build for Linux
+GOOS=linux GOARCH=amd64 go build -o "$BUILD_DIR/$EXECUTABLE_NAME_LINUX"
 if [ $? -eq 0 ]; then
-    echo "Build successful: $BUILD_DIR/$EXECUTABLE_NAME"
+    echo "Build successful for Linux: $BUILD_DIR/$EXECUTABLE_NAME_LINUX"
 else
-    echo "Build failed"
+    echo "Build failed for Linux"
+    exit 1
+fi
+
+# Build for Windows
+GOOS=windows GOARCH=amd64 go build -o "$BUILD_DIR/$EXECUTABLE_NAME_WINDOWS"
+if [ $? -eq 0 ]; then
+    echo "Build successful for Windows: $BUILD_DIR/$EXECUTABLE_NAME_WINDOWS"
+else
+    echo "Build failed for Windows"
     exit 1
 fi
